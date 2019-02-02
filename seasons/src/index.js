6f67f416eb8bc400f4882119a58818b4d06d1377
Pranav <pranav.k.vema@technossus.com>
2019-02-02 20:39:18 +0530

    Using States in react
    Impemented state in react and some conditionally rendering in jsx

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -10,12 +10,36 @@ import ReactDOM from 'react-dom';
 // };
 
 class App extends React.Component {
+
+    constructor(props) {
+            super(props);
+            // this is the only time we do direct assignment to this state
+            this.state = {lat: null, errorMessage: ''};
+
+            window.navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    // We called setstate!!!!
+                    this.setState({lat: position.coords.latitude});
+                },
+                err => {
+                    this.setState({errorMessage: err.message})
+                }
+            );
+    }
+    // React says we have to define render !! this is by default required 
     render() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err) 
-        );
-        return <div> Latitude: </div>
+        // return <div> Latitude: {this.state.lat} <br/>
+        //              Error: {this.state.errorMessage} 
+        //        </div>
+
+        if(this.state.errorMessage && !this.state.lat) {
+            return <div> Error: {this.state.errorMessage} </div>
+        }
+        if(!this.state.errorMessage && this.state.lat) {
+            return <div> Latitude: {this.state.lat} </div>
+        }
+         return <div> Loading........ </div>
+       
     }
 }
 
